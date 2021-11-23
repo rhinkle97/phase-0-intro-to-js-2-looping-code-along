e9ea45ca4ee7bfd6ed8c531e7b03a4e5cd0dd0cd
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-11-23 15:58:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardsTo = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards() {
+    for (let i = 0; i < cardsTo.length; i++) {
+        console.log('Thank you,' + ' ' + cardsTo[i] + ', for the wonderful surprise gift!');
+        debugger;
+    }
+    return Array(3);
+};
+
+
+
+function countDown() {
+    for (let i = 0; i < 11; i++) {
+        console.log(i);
+    }
+};
